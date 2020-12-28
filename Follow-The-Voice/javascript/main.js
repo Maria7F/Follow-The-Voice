75ba0b8a1c74ca330e9372774e6449e13c65e192
MariaFelemban <maria7.felemban@gmail.com>
2020-12-28 14:09:45 +0300

    at player page the + - signs are now working

--- Follow-The-Voice/javascript/main.js
+++ Follow-The-Voice/javascript/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+    var input_player_name = $("player-name");
+
+
+    $(add).click(function(e) {
+        e.preventDefault();
+        $("#player-name-form").append('<p id="players">Player name: <input id="player-name"></p>')
+
+    });
+
+    $("#delete").on("click", function() {
+        $("#players").remove();
+    });
+
+
 });
