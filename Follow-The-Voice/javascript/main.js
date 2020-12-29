ed38a47fdbc81670b2b951ab354ecdf92350d83c
MariaFelemban <maria7.felemban@gmail.com>
2020-12-29 19:47:48 +0300

    all working exept the pause button

--- Follow-The-Voice/javascript/main.js
+++ Follow-The-Voice/javascript/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-
+    // for the players page ======================
     var input_player_name = $("player-name");
-
-
-    $(add).click(function(e) {
+    $("#add").click(function(e) {
         e.preventDefault();
         $("#player-name-form").append('<p id="players">Player name: <input id="player-name"></p>')
 
@@ -13,5 +11,39 @@ $(document).ready(function() {
         $("#players").remove();
     });
 
+    //============================================
+    // for the start-game-page ======================
 
+    var name = $("#player-name").val();
+    var names = new Array(name);
+    console.log(name);
+
+    $("#test").on("click", function() {
+        var playlist = ['/audio/1.mp3', '/audio/2.mp3', '/audio/3.mp3', '/audio/4.mp3'];
+        var words = ['جمل ماشي', 'عالمماشي', 'جيت اضمه', 'خطف شاشي'];
+        //reference --> https://stackoverflow.com/questions/36315320/jquery-animate-changing-text-infinite-loop
+        var index = 0;
+        setInterval(function() {
+            $("#game-words").animate({
+                opacity: 0
+            }, function() {
+                if (words.length > index) {
+                    var x = Math.floor(Math.random() * 4);
+                    $(this).text(words[index]).animate({ opacity: 1 })
+                    $("#audio").attr("src", playlist[x]);
+                    var audio = new Audio(playlist[x]).play();
+                    x++;
+                    index++;
+                } else
+                    index = 0;
+            });
+        }, 2000);
+
+    });
 });
+
+// var x = Math.floor(Math.random() * 4);
+// var y = Math.floor(Math.random() * 4);
+//$('#game-words').text(words[y]);
+// $("#audio").attr("src", playlist[x]);
+// var audio = new Audio(playlist[x]).play();
